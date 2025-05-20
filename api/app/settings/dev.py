ecc18de0053b67bc3a2f60fa63643b271d64fb9d
Ashish Kapoor <swiftobjc@gmail.com>
2025-05-20 23:30:02 +0530

    fix: cors issue

--- api/app/settings/dev.py
+++ api/app/settings/dev.py
@@ -1,7 +1,21 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1"]
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+# Development CORS settings
+CORS_ALLOW_ALL_ORIGINS = True  # Only use this in development!
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
